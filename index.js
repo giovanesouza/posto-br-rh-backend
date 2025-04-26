368396a66cb2d514fedf2980b2bac6306190a7b1
Giovane Souza <developergiovanesouza@gmail.com>
2025-04-26 08:45:48 -0300

    test origin

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.use(cors(
     {
-        origin: process.env.CORS_ORIGIN_URL,
+        origin: "*",
         methods: ["GET", "POST", "PUT", "PATCH", "DELETE"]
     }
 ));
