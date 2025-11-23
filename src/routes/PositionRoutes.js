4b35813500fce8136315d59f32555d566baa7e0d
Giovane Souza <developergiovanesouza@gmail.com>
2025-11-22 21:15:09 -0300

    refactor: update auth reference

--- src/routes/PositionRoutes.js
+++ src/routes/PositionRoutes.js
@@ -1,16 +1,16 @@
 import { Router } from 'express';
 import { PositionController } from '../controllers/PositionController.js';
 import { FieldValidations } from '../middleware/fieldsValidationsMiddleware.js';
-import authorization from '../auth/authorization.js';
+import authorize from '../auth/authorization.js';
 
 const positionRoutes = Router();
 const positionController = new PositionController();
 const validation = new FieldValidations();
 
-positionRoutes.post('/positions', authorization, validation.validatePositionData, positionController.createPosition);
-positionRoutes.get('/positions', authorization, positionController.findAllPositions);
-positionRoutes.get('/positions/:id', authorization, validation.validateIdParameter, positionController.findPositionById);
-positionRoutes.put('/positions/:id', authorization, validation.validateIdParameter, positionController.updatePosition);
-positionRoutes.delete('/positions/:id', authorization, validation.validateIdParameter, positionController.deleteposition);
+positionRoutes.post('/positions', authorize(), validation.validatePositionData, positionController.createPosition);
+positionRoutes.get('/positions', authorize(), positionController.findAllPositions);
+positionRoutes.get('/positions/:id', authorize(), validation.validateIdParameter, positionController.findPositionById);
+positionRoutes.put('/positions/:id', authorize(), validation.validateIdParameter, positionController.updatePosition);
+positionRoutes.delete('/positions/:id', authorize(), validation.validateIdParameter, positionController.deleteposition);
 
 export { positionRoutes };
